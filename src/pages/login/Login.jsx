60b7087a4bce7895a11886cde4f5f98d2559fc8e
nii_okantey <nunoogideon19@gmail.com>
2023-05-11 12:35:23 +0000

    login done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,49 @@
 import React from "react";
-import blob from "../../images/blob.png";
+import blob from "../../images/blob.jpg";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <section>
-      <div>
-        <h1>Welcome Back!</h1>
+    <section
+      className="fixed h-screen w-screen overflow-hidden text-white"
+      style={{
+        background: `url(${blob})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div className="h-full flex flex-col justify-center w-[30%] m-auto ">
+        <h1 className="font-volkhov font-bold text-2xl text-center my-2">
+          Welcome Back!
+        </h1>
         <form>
-          <div>
-            <label htmlFor="email">Username</label>
-            <input type="email" name="email" />
+          <div className="flex flex-col w-full ">
+            <label htmlFor="email" className="font-bold">
+              Email
+            </label>
+            <input
+              className="w-full pl-3 py-2 outline-none border border-primaryLight mb-2 rounded-md"
+              type="email"
+              name="email"
+            />
           </div>
-          <div>
-            <label htmlFor="password">Username</label>
-            <input type="password" name="password" />
+          <div className="flex flex-col w-full">
+            <label htmlFor="password" className="font-bold">
+              Password
+            </label>
+            <input
+              className="w-full pl-3 py-2 outline-none border border-primaryLight mb-2 rounded"
+              type="password"
+              name="password"
+            />
           </div>
-          <div>
-            <input type="submit" value="Login" />
+          <div className="flex w-full">
+            <input
+              className="w-full bg-primary text-white font-bold py-3 my-4 rounded"
+              type="submit"
+              value="Login"
+            />
           </div>
         </form>
       </div>
