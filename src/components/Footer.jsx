9fdb554fa642d730f566472dccabe625ff760fae
nii_okantey <nunoogideon19@gmail.com>
2023-05-13 06:28:26 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { styled } from "styled-components";
+import {
+  BsInstagram,
+  BsTwitter,
+  BsFacebook,
+  BsMessenger,
+} from "react-icons/bs";
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <FooterContentLeft>
+      <FooterContent>
         <h1>Letelect.</h1>
         <p>
           Our mission is to make the voting process more accessible and
@@ -12,9 +18,18 @@ const Footer = () => {
           security and integrity. Join us in shaping the future of democracy by
           voting online with confidence and ease.
         </p>
-      </FooterContentLeft>
-      <FooterContentMiddle></FooterContentMiddle>
-      <FooterContentRight></FooterContentRight>
+        <Social className="cinder__footer-links_icons">
+          <BsInstagram size={30} color="var(--bg-blue)" />
+          <BsTwitter size={30} color="var(--bg-blue)" />
+          <BsFacebook size={30} color="var(--bg-blue)" />
+          <BsMessenger size={30} color="var(--bg-blue)" />
+        </Social>
+        <Copyright className="cinder__footer-copyright">
+          <span>COPYRIGHT © 2023 </span>
+          <span className="cinder__footer-copyright-cinder">Letelect</span>
+          <span> ALL RIGHTS RESERVED.</span>
+        </Copyright>
+      </FooterContent>
     </FooterWrapper>
   );
 };
@@ -23,7 +38,13 @@ const FooterWrapper = styled.footer`
   background: var(--text-black);
 `;
 
-const FooterContentLeft = styled.div`
+const FooterContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-block: 1.5rem;
   & h1 {
     font-family: var(--font-dance);
     color: var(--text-white);
@@ -32,9 +53,29 @@ const FooterContentLeft = styled.div`
   }
   & p {
     color: var(--text-white);
+    width: 60%;
+    text-align: center;
+    padding-block: 0.5rem;
   }
 `;
 
-const FooterContentMiddle = styled.div``;
-const FooterContentRight = styled.div``;
+const Social = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding-block: 1rem;
+`;
+const Copyright = styled.div`
+  font-weight: bold;
+  color: var(--text-white);
+  text-align: center;
+  margin-top: 1rem;
+  & :nth-child(2) {
+    color: var(--bg-blue);
+    font-size: 1rem;
+  }
+`;
 export default Footer;
