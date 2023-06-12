c215de809a98cf9a6b1bdb7815d0bb8e67c76b03
nii_okantey <nunoogideon19@gmail.com>
2023-06-12 13:26:53 +0000

    updates

--- src/pages/Positions.jsx
+++ src/pages/Positions.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+import { Header, Sidebar, BarChart } from "../components";
+import { Parent, Content, MainContent, View, Intro } from "../pages/Dashboard";
 
 const Positions = () => {
-  return <div>Positions</div>;
+  return (
+    <Parent>
+      <Content>
+        <Sidebar />
+        <MainContent>
+          <Header />
+          <View></View>
+        </MainContent>
+      </Content>
+    </Parent>
+  );
 };
 
 export default Positions;
