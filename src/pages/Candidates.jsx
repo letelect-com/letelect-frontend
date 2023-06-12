0034f8af7a59c0c0fcb3cd9593439e63017f797b
nii_okantey <nunoogideon19@gmail.com>
2023-06-12 14:49:50 +0000

    candidates

--- src/pages/Candidates.jsx
+++ src/pages/Candidates.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import styled from "styled-components";
 import { Header, Sidebar, BarChart } from "../components";
 import { Parent, Content, MainContent, View, Intro } from "../pages/Dashboard";
+import { Button } from "../components/Navbar";
 
 const Candidates = () => {
   return (
@@ -10,7 +11,19 @@ const Candidates = () => {
         <Sidebar />
         <MainContent>
           <Header />
-          <View></View>
+          <View>
+            <Intro>
+              <div>
+                <h2>Candidates</h2>
+                <p>Add new candidates</p>
+              </div>
+              <div>
+                <Button dashboard>
+                  <span>Add new candidate</span>
+                </Button>
+              </div>
+            </Intro>
+          </View>
         </MainContent>
       </Content>
     </Parent>
