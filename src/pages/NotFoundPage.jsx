de54336975c1b88caac5ed731c7fbc1b8b24f5a8
nii_okantey <nunoogideon19@gmail.com>
2023-06-13 09:32:31 +0000

    404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return <div>
+    <h1>The Page you're looking for does not exist</h1>
+  </div>;
 };
 
 export default NotFoundPage;
