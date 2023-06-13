3a48e2e30cd24df1f86f11ae8e153a1d09ca1501
nii_okantey <nunoogideon19@gmail.com>
2023-06-13 09:32:59 +0000

    updates

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>
-    <h1>The Page you're looking for does not exist</h1>
-  </div>;
+  return (
+    <div>
+      <h1>The Page you're looking for does not exist</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam minima
+        rerum tempore libero enim cumque totam animi nisi aut maiores architecto
+        accusantium, optio mollitia repudiandae vel sint earum modi.
+      </p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
